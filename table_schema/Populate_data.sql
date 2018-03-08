8fbaa746f83ce7a02594dcdf748fff6fb3fe9568
Unknown <swan0005@student.monash.edu>
2018-03-08 16:15:07 +1100

    initial DB scripts

---
+++ table_schema/Populate_data.sql
@@ -0,0 +1,12 @@
+--------------------------------------Populate Table <<Resident>>-------------------------------------
+INSERT INTO RESIDENT (FIRSTNAME,SURENAME,DOB,ADDRESS,POSTCODE,EMAIL,MOBILE,NUMBEROFRESIDENT,ENERGYPROVIDER) VALUES ('Tom','Hanks','1978-01-01','14 Brixton ave,Eltham North',3163,'tom.hanks@monash.edu','0448129762',3,'Red Energy');
+INSERT INTO RESIDENT (FIRSTNAME,SURENAME,DOB,ADDRESS,POSTCODE,EMAIL,MOBILE,NUMBEROFRESIDENT,ENERGYPROVIDER) VALUES ('Voljin','Grim','1981-08-21','37A Marriot St,Caulfield',3062,'voljin.grim@monash.edu','0449121762',1,'AGL Energy');
+INSERT INTO RESIDENT (FIRSTNAME,SURENAME,DOB,ADDRESS,POSTCODE,EMAIL,MOBILE,NUMBEROFRESIDENT,ENERGYPROVIDER) VALUES ('William','Wang','1990-01-08','5/32 Royal ave,Glen Huntly',3162,'swan0005@student.monash.edu','0449884513',2,'Red Energy');
+INSERT INTO RESIDENT (FIRSTNAME,SURENAME,DOB,ADDRESS,POSTCODE,EMAIL,MOBILE,NUMBEROFRESIDENT,ENERGYPROVIDER) VALUES ('Silvanas','Churhchill','1989-06-11','22 Bourke Road,City',3000,'schu0005@student.monash.edu','0448187762',1,'Commander');
+INSERT INTO RESIDENT (FIRSTNAME,SURENAME,DOB,ADDRESS,POSTCODE,EMAIL,MOBILE,NUMBEROFRESIDENT,ENERGYPROVIDER) VALUES ('Peter','Cruise','1985-11-11','6/41 Dandenong Road,Dockland',3002,'pCru0100@student.monash.edu','0449516788',2,'Red Energy');
+----------------------------------End Populate Table <<Resident>>--------------------------------------
+
+----------------------------------Populate Table <<Credential>>----------------------------------------
+INSERT INTO RESIDENT (USERNAME,RESID,PASSWORDHASH,REGISTRATIONDATE) VALUES ('tom0001',1,'1978-01-01','14 Brixton ave,Eltham North',3163,'tom.hanks@monash.edu','0448129762',3,'Red Energy');
+
+-------------------------------End Populate Table <<Credential>>---------------------------------------
