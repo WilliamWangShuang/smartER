8961c0910f157122b54ddfa1007e0ecd401f9d73
Unknown <swan0005@student.monash.edu>
2018-03-08 18:12:23 +1100

    create webservice project

---
+++ SmartER-war/src/java/smartEREntities/Electricity.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartEREntities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author William
+ */
+@Entity
+@Table(name = "ELECTRICITY")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Electricity.findAll", query = "SELECT e FROM Electricity e")
+    , @NamedQuery(name = "Electricity.findByUsageid", query = "SELECT e FROM Electricity e WHERE e.usageid = :usageid")
+    , @NamedQuery(name = "Electricity.findByUsagedate", query = "SELECT e FROM Electricity e WHERE e.usagedate = :usagedate")
+    , @NamedQuery(name = "Electricity.findByUsagehour", query = "SELECT e FROM Electricity e WHERE e.usagehour = :usagehour")
+    , @NamedQuery(name = "Electricity.findByFridgeusage", query = "SELECT e FROM Electricity e WHERE e.fridgeusage = :fridgeusage")
+    , @NamedQuery(name = "Electricity.findByAcusage", query = "SELECT e FROM Electricity e WHERE e.acusage = :acusage")
+    , @NamedQuery(name = "Electricity.findByWmusage", query = "SELECT e FROM Electricity e WHERE e.wmusage = :wmusage")
+    , @NamedQuery(name = "Electricity.findByTemperature", query = "SELECT e FROM Electricity e WHERE e.temperature = :temperature")})
+public class Electricity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "USAGEID")
+    private Integer usageid;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "USAGEDATE")
+    @Temporal(TemporalType.DATE)
+    private Date usagedate;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "USAGEHOUR")
+    private int usagehour;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "FRIDGEUSAGE")
+    private BigDecimal fridgeusage;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ACUSAGE")
+    private BigDecimal acusage;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "WMUSAGE")
+    private BigDecimal wmusage;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "TEMPERATURE")
+    private int temperature;
+    @JoinColumn(name = "RESID", referencedColumnName = "RESID")
+    @ManyToOne(optional = false)
+    private Resident resid;
+
+    public Electricity() {
+    }
+
+    public Electricity(Integer usageid) {
+        this.usageid = usageid;
+    }
+
+    public Electricity(Integer usageid, Date usagedate, int usagehour, BigDecimal fridgeusage, BigDecimal acusage, BigDecimal wmusage, int temperature) {
+        this.usageid = usageid;
+        this.usagedate = usagedate;
+        this.usagehour = usagehour;
+        this.fridgeusage = fridgeusage;
+        this.acusage = acusage;
+        this.wmusage = wmusage;
+        this.temperature = temperature;
+    }
+
+    public Integer getUsageid() {
+        return usageid;
+    }
+
+    public void setUsageid(Integer usageid) {
+        this.usageid = usageid;
+    }
+
+    public Date getUsagedate() {
+        return usagedate;
+    }
+
+    public void setUsagedate(Date usagedate) {
+        this.usagedate = usagedate;
+    }
+
+    public int getUsagehour() {
+        return usagehour;
+    }
+
+    public void setUsagehour(int usagehour) {
+        this.usagehour = usagehour;
+    }
+
+    public BigDecimal getFridgeusage() {
+        return fridgeusage;
+    }
+
+    public void setFridgeusage(BigDecimal fridgeusage) {
+        this.fridgeusage = fridgeusage;
+    }
+
+    public BigDecimal getAcusage() {
+        return acusage;
+    }
+
+    public void setAcusage(BigDecimal acusage) {
+        this.acusage = acusage;
+    }
+
+    public BigDecimal getWmusage() {
+        return wmusage;
+    }
+
+    public void setWmusage(BigDecimal wmusage) {
+        this.wmusage = wmusage;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public Resident getResid() {
+        return resid;
+    }
+
+    public void setResid(Resident resid) {
+        this.resid = resid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usageid != null ? usageid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Electricity)) {
+            return false;
+        }
+        Electricity other = (Electricity) object;
+        if ((this.usageid == null && other.usageid != null) || (this.usageid != null && !this.usageid.equals(other.usageid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "smartEREntities.Electricity[ usageid=" + usageid + " ]";
+    }
+    
+}
---
+++ SmartER-war/src/java/smartEREntities/Resident.java
@@ -0,0 +1,243 @@
+package smartEREntities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author William
+ */
+@Entity
+@Table(name = "RESIDENT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Resident.findAll", query = "SELECT r FROM Resident r")
+    , @NamedQuery(name = "Resident.findByResid", query = "SELECT r FROM Resident r WHERE r.resid = :resid")
+    , @NamedQuery(name = "Resident.findByFirstname", query = "SELECT r FROM Resident r WHERE r.firstname = :firstname")
+    , @NamedQuery(name = "Resident.findBySurename", query = "SELECT r FROM Resident r WHERE r.surename = :surename")
+    , @NamedQuery(name = "Resident.findByDob", query = "SELECT r FROM Resident r WHERE r.dob = :dob")
+    , @NamedQuery(name = "Resident.findByAddress", query = "SELECT r FROM Resident r WHERE r.address = :address")
+    , @NamedQuery(name = "Resident.findByPostcode", query = "SELECT r FROM Resident r WHERE r.postcode = :postcode")
+    , @NamedQuery(name = "Resident.findByEmail", query = "SELECT r FROM Resident r WHERE r.email = :email")
+    , @NamedQuery(name = "Resident.findByMobile", query = "SELECT r FROM Resident r WHERE r.mobile = :mobile")
+    , @NamedQuery(name = "Resident.findByNumberofresident", query = "SELECT r FROM Resident r WHERE r.numberofresident = :numberofresident")
+    , @NamedQuery(name = "Resident.findByEnergyprovider", query = "SELECT r FROM Resident r WHERE r.energyprovider = :energyprovider")})
+public class Resident implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "RESID")
+    private Integer resid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "SURENAME")
+    private String surename;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "DOB")
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "ADDRESS")
+    private String address;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "POSTCODE")
+    private short postcode;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 12)
+    @Column(name = "MOBILE")
+    private String mobile;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "NUMBEROFRESIDENT")
+    private int numberofresident;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "ENERGYPROVIDER")
+    private String energyprovider;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "resid")
+    private Collection<Credential> credentialCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "resid")
+    private Collection<Electricity> electricityCollection;
+
+    public Resident() {
+    }
+
+    public Resident(Integer resid) {
+        this.resid = resid;
+    }
+
+    public Resident(Integer resid, String firstname, String surename, Date dob, String address, short postcode, String email, String mobile, int numberofresident, String energyprovider) {
+        this.resid = resid;
+        this.firstname = firstname;
+        this.surename = surename;
+        this.dob = dob;
+        this.address = address;
+        this.postcode = postcode;
+        this.email = email;
+        this.mobile = mobile;
+        this.numberofresident = numberofresident;
+        this.energyprovider = energyprovider;
+    }
+
+    public Integer getResid() {
+        return resid;
+    }
+
+    public void setResid(Integer resid) {
+        this.resid = resid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public short getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(short postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getNumberofresident() {
+        return numberofresident;
+    }
+
+    public void setNumberofresident(int numberofresident) {
+        this.numberofresident = numberofresident;
+    }
+
+    public String getEnergyprovider() {
+        return energyprovider;
+    }
+
+    public void setEnergyprovider(String energyprovider) {
+        this.energyprovider = energyprovider;
+    }
+
+    @XmlTransient
+    public Collection<Credential> getCredentialCollection() {
+        return credentialCollection;
+    }
+
+    public void setCredentialCollection(Collection<Credential> credentialCollection) {
+        this.credentialCollection = credentialCollection;
+    }
+
+    @XmlTransient
+    public Collection<Electricity> getElectricityCollection() {
+        return electricityCollection;
+    }
+
+    public void setElectricityCollection(Collection<Electricity> electricityCollection) {
+        this.electricityCollection = electricityCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (resid != null ? resid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Resident)) {
+            return false;
+        }
+        Resident other = (Resident) object;
+        if ((this.resid == null && other.resid != null) || (this.resid != null && !this.resid.equals(other.resid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "smartEREntities.Resident[ resid=" + resid + " ]";
+    }
+    
+}
